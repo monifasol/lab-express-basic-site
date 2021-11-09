d5b71f3ae45509f6feebf7db79e07150c46fbbba
Monica Saiz <moni.sm@gmail.com>
2021-11-09 17:41:13 +0100

    Lab Express Basic Site

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "")))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.all("/home",  (request, response)=> {
+    response.sendFile(path.join(__dirname, "views", "home.html"))
+})
+
+app.all("/about",  (request, response)=> {
+    response.sendFile(path.join(__dirname, "views", "about.html"))
+})
+
+app.all("/works",  (request, response)=> {
+    response.sendFile(path.join(__dirname, "views", "works.html"))
+})
+
+app.all("/gallery",  (request, response)=> {
+    response.sendFile(path.join(__dirname, "views", "gallery.html"))
+})
+
+// start the application
+app.listen(3002, ()=> {
+    console.log("Yuhuuu! I'm running!")
+})
